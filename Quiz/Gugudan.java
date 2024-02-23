636729d3656f21222194ac15445e904980b590a9
lmj0228 <160682324+lmj0228@users.noreply.github.com>
2024-02-23 13:11:17 +0900

    Add files via upload

---
+++ Quiz/Gugudan.java
@@ -0,0 +1,12 @@
+package Quiz;
+
+public class Gugudan {
+
+	public void print(int i) {
+		System.out.println(i + "단");
+		
+		for(int j = 1; j < 10; j++) {
+			System.out.println(i + "*" + j + "=" + i*j);
+		}	
+	}
+}
