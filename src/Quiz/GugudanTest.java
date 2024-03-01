90454917326222c6090143532d7e7e746e23d390
lmj0228 <160682324+lmj0228@users.noreply.github.com>
2024-03-01 18:41:00 +0900

    Update GugudanTest.java

--- src/Quiz/GugudanTest.java
+++ src/Quiz/GugudanTest.java
@@ -1,30 +1,18 @@
 package Quiz;
 
-import java.util.Scanner;
-
 public class GugudanTest {
-	
-	public static void main(String[] args) {
-		
-		Gugudan ggd = new Gugudan();
-		
-		ggd.printHorizontal();
-		
-		ggd.printColumn(4);
-		Scanner scanner = new Scanner(System.in);
-		
-		ggd.print(3);
-		
-		System.out.print("숫자를 입력하세요.");
-		int num;
-		
-		while((num = scanner.nextInt()) <10) {
-			ggd.print(num);
-			System.out.print("숫자를 입력하세요.");
-		}
-	
-		System.out.println("-".repeat(15));
-		
-		System.out.println("End");
-	}
+    
+    public static void main(String[] args) {
+        
+        Gugudan ggd = new Gugudan();
+        
+        ggd.print(4);
+        System.out.println("=".repeat(50));
+        
+        ggd.printHorizontal();
+        System.out.println("=".repeat(50));
+        
+        ggd.printColumn(3);
+    }
 }
+
